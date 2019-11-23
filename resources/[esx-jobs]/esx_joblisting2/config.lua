7f151f9e721a53cd573704f7083276c6afd13df6
Mirrox1337 <anton.per.larsson@gmail.com>
2019-11-23 06:38:01 +0100

    Platsbanken (Arbetsförmedling med discord webhook)

---
+++ resources/[esx-jobs]/esx_joblisting2/config.lua
@@ -0,0 +1,24 @@
+Config              = {}
+Config.DrawDistance = 10.0
+Config.ZoneSize     = {x = 2.0, y = 2.0, z = 2.0}
+Config.MarkerColor  = {r = 0, g = 255, b = 0}
+Config.MarkerType   = 27
+Config.Locale = 'sv'
+
+Config.Zones = {
+	{x = -265.0, y = -963.6, z = 31.22}
+}
+
+Config.Webhook = {
+	police = "https://discordapp.com/api/webhooks/647667643150630913/zFGRK8gkhpjZYITDY-4149YOncvqo-YGlQIiofdrJMIpWOdC3l5fFLQMthL3cA25WxOH",
+	ambulance = "https://discordapp.com/api/webhooks/647667286576070692/PBi6bA1AsMP1C2dpNQjPFG0AfGwfEo8dQwnESoLdqN4Hk2lbE5j7w3SnkaLH_lSC7-bd",
+	bennys = "https://discordapp.com/api/webhooks/647668407889821716/XIoAI3f-2F4FhuTQjXW5cAYmVsnvAAq8BjmFRBm5kJBS_RrxJl8ySxIO6K-OoqDdJv6S",
+	cardealer = "https://discordapp.com/api/webhooks/647669082606403590/PZyzOAI0LLVRCl5SL6ScBv_phliv5-b7_NhvBj0kV1I0ghXP65dyw184vZf6329mse0C",
+	mechanic = "https://discordapp.com/api/webhooks/647668702380163073/X_QVhXBvUsYSj-mRhpJB1y4MXWoPVsN-PYDvsnetLD36OhAPhAzzW9665qmDQeKQxyC2",
+	realestate = "realestate",
+	taxi = "https://discordapp.com/api/webhooks/647668156931768343/r0XhjWtzT8Mk05r6KdgqfNaNsgfb8QO7mt-ax8okR5lh6ynlfWYO8ed2o37F-xrwNXCU",
+	delivery = "delivery",
+	galaxy = "https://discordapp.com/api/webhooks/647669366917300234/A_D3VwtB3BU3E3n69rD7DI9CzVMGP_PiT53hiLjTPeaTA75gY40SuvQSCuDNurQWAqSx",
+	journalist = "https://discordapp.com/api/webhooks/647669629636050975/0AVMCUCVl8Kobm48snBMenZmJDFLae24CpAA73vtRuXx8CYXUjf6U0sg8mGDr0Q0V-qj",
+	securitas = "https://discordapp.com/api/webhooks/647666826217521156/hkqj80AYgLNJ2aet1-FNsYQnNGF3iPSSqXSVlJPxPeR6A8I6eysOq9_STVif1UFJy4tz"
+}
