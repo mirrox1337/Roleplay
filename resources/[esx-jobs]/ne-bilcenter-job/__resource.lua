9796b5b388367131c03a77f2382ecdb88f94b210
chronohub <55539112+chronohub@users.noreply.github.com>
2019-11-23 08:37:39 +0100

    Tagit bort translate filer andra språk

--- resources/[esx-jobs]/ne-bilcenter-job/__resource.lua
+++ resources/[esx-jobs]/ne-bilcenter-job/__resource.lua
@@ -7,24 +7,14 @@ version '1.1.0'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
 	'locales/sv.lua',
-	'locales/pl.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/br.lua',
-	'locales/en.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
 	'locales/sv.lua',
-	'locales/pl.lua',
 	'config.lua',
 	'client/utils.lua',
 	'client/main.lua'
