4a4b94483be506c8636589429ef68f197c778275
chronohub <55539112+chronohub@users.noreply.github.com>
2019-11-24 09:58:58 +0100

    IPL, Domstol YMAP & Portaler

---
+++ resources/[scripts]/ne-portals/config.lua
@@ -0,0 +1,23 @@
+Config                            = {}
+
+Config.Teleporters = {
+	['Domstol'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 232.95, 
+			['y'] =  -410.94, 
+			['z'] = 47.11,
+			['Information'] = '[E] Gå in',
+		},
+		['Exit'] = {
+			['x'] = 236.53, 
+			['y'] = -400.45, 
+			['z'] = -85.93, 
+			['Information'] = '[E] Gå ut' 
+		}
+	}
+
+	-- Nästa här
+}
+
+
