c62b2a99d542992da6a5ce34c618cadd0917a0b0
chronohub <55539112+chronohub@users.noreply.github.com>
2019-12-04 14:33:15 +0100

    tid/väder sync, jsfour-register styling, tattooshop coords fixade

--- resources/[esx]/esx_tattooshop/config.lua
+++ resources/[esx]/esx_tattooshop/config.lua
@@ -5,13 +5,13 @@ Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
 Config.Color        = {r = 0, g = 128, b = 255}
 Config.Type         = 1
 
-Config.Locale       = 'en'
+Config.Locale       = 'sv'
 
 Config.Zones = {
-	vector3(1322.6, -1651.9, 50.3),
+	vector3(1322.6, -1651.9, 51.28),
 	vector3(-1153.6, -1425.6, 4.0),
-	vector3(322.1, 180.4, 102.0),
+	vector3(322.1, 180.4, 102.60),
 	vector3(-3170.0, 1075.0, 20.0),
 	vector3(1864.6, 3747.7, 32.0),
-	vector3(-293.7, 6200.0, 31.4)
+	vector3(-293.7, 6200.0, 30.50)
 }
