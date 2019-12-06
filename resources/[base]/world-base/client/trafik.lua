9ebfccc0f3a028a5b3d7037355406608207fd3ab
Mirrox1337 <anton.per.larsson@gmail.com>
2019-12-06 08:09:10 +0100

    Mindra galna NPCer, Optimeringar, Andra base functioner

--- resources/[base]/world-base/client/trafik.lua
+++ resources/[base]/world-base/client/trafik.lua
@@ -1,10 +1,12 @@
 --Multiplier f�r trafik
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(100)		
-			SetVehicleDensityMultiplierThisFrame(0.05)
-			SetRandomVehicleDensityMultiplierThisFrame(0.1)
-			SetParkedVehicleDensityMultiplierThisFrame(0.001)
-			Citizen.Wait(1)
+		Citizen.Wait(10)		
+		SetVehicleDensityMultiplierThisFrame(0.1)
+		SetPedDensityMultiplierThisFrame(0.3)
+		SetRandomVehicleDensityMultiplierThisFrame(0.2)
+		SetParkedVehicleDensityMultiplierThisFrame(0.0)
+		SetScenarioPedDensityMultiplierThisFrame(0.2, 0.2)
+		Citizen.Wait(10)
 	end
 end)
