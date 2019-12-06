7d384df66e287de421724a9bedd5da1ed9b1bb83
chronohub <55539112+chronohub@users.noreply.github.com>
2019-12-06 02:03:32 +0100

    sv_licenseKey

--- run.cmd
+++ run.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-files\run.cmd +exec server.cfg +set onesync_enabled 1 +sv_licenseKey 1qx8cklbgnkkdu2qlyslai0kz8z82ug4
+files\run.cmd +exec server.cfg +set onesync_enabled 1 +sv_licenseKey ghxtg4su7o18xpu8avpdtxxzt0p1b7e5
