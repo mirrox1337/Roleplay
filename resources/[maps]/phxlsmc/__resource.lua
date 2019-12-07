052a8718f7af9d08ae9c976c7d6884be6cdfb158
Mirrox1337 <anton.per.larsson@gmail.com>
2019-12-07 23:43:17 +0100

    ClearAreaOfPeds på sjukhuset

--- resources/[maps]/phxlsmc/__resource.lua
+++ resources/[maps]/phxlsmc/__resource.lua
@@ -1,2 +1,6 @@
+client_scripts {
+    'client.lua'
+}
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 this_is_a_map 'yes'
---
+++ resources/[maps]/phxlsmc/client.lua
@@ -0,0 +1,6 @@
+Citizen.CreateThread(function()
+    while (true) do
+        ClearAreaOfPeds(342.44, -587.00, 27.79, 50.0, 1)
+        Citizen.Wait(0)
+    end
+end)
