621dc7a7ce4dbeb8adc4f9862eaa447f998fd804
Mirrox1337 <anton.per.larsson@gmail.com>
2019-12-08 16:55:41 +0100

    Citizen.Wait(1000)	--> Citizen.Wait(100) på trafik

--- resources/[base]/world-base/client/trafik.lua
+++ resources/[base]/world-base/client/trafik.lua
@@ -1,13 +1,13 @@
 --Multiplier f�r trafik
 Citizen.CreateThread(function()
 	while true do
-		Citizen.Wait(1000)		
+		Citizen.Wait(100)		
 		SetVehicleDensityMultiplierThisFrame(0.01)
 		SetPedDensityMultiplierThisFrame(0.3)
 		SetRandomVehicleDensityMultiplierThisFrame(0.0)
 		SetNumberOfParkedVehicles(0)
 		SetParkedVehicleDensityMultiplierThisFrame(0.0)
 		SetScenarioPedDensityMultiplierThisFrame(0.01, 0.01)
-		Citizen.Wait(1000)
+		Citizen.Wait(100)
 	end
 end)
