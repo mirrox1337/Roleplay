adaedede3e0d554b50421ffa900556e80b343054
Mirrox1337 <anton.per.larsson@gmail.com>
2019-12-15 14:45:50 +0100

    fix after förre commiten

---
+++ resources/[disc]/disc-inventoryhud/html/locales/sv.js
@@ -0,0 +1,9 @@
+var invLocale = new Object();
+invLocale.dropItem = "Betala";
+invLocale.useItem = "Använd";
+invLocale.giveItem = "Ge";
+invLocale.label = "Inventory Label";
+invLocale.stealmoney = "Stjäl pengar";
+invLocale.seizemoney = "Beslagta pengar";
+invLocale.takemoney = "Ta";
+invLocale.storemoney = "Lagra";
---
+++ resources/[disc]/disc-inventoryhud/locales/cs.lua
@@ -0,0 +1,14 @@
+﻿Locales['cs'] = {
+    -- Client side
+       ['used'] = "Item Used",
+       ['keyshop'] = "Press ~INPUT_CONTEXT~ to open Shop",
+       ['keystash'] = "Press ~INPUT_CONTEXT~ to open Stash",
+   -- Server side
+       ['drop'] = "Drop",
+       ['glove'] = "Glove Box",
+       ['player'] = "Player",
+       ['shop'] = "Shop",
+       ['stash'] = "Stash",
+       ['trunk'] = "Trunk-",
+       ['added'] = "Item Added",
+   } 
