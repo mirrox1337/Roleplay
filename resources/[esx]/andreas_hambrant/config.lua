a44d3916c2894e7b5ade39721db3fb66e396e6f0
andreas-developing <41629543+andreas-developing@users.noreply.github.com>
2019-12-18 17:11:16 +0100

    Lade till hembränt script
    Man kan koka osv

---
+++ resources/[esx]/andreas_hambrant/config.lua
@@ -0,0 +1,28 @@
+Config = {}
+
+--== INGREDIENSER SOM BEHÖVS FÖR ATT KOKA ==--
+Config.ItemsNeeded = { 
+    'turbojast',
+    'socker',
+    'vatten',
+    'kol',
+}
+
+--== HUR MYCKET DET SKA KOSTA ATT KÖPA INGREDIENSERNA ==--
+Config.BuyCost = 1500
+
+--== HUR LÅNG TID DET SKA TA ATT KOKA ALLTING
+Config.CookTime = 10 * 1000
+
+--==  HUR MÅNGA LITER MAN SKA FÅ ==--
+Config.RecieveAmount = 5
+
+--== AFFÄREN DÄR MAN KÖPER INGREDIENSERNA ==--
+Config.PickupPoints = {
+    { x = 1332.681, y = 4325.132, z = 38.2534 },
+}
+
+--== KOKPUNKTER, LÄGG TILL FLERA GENOM FLER KOORDINATER ==--
+Config.CookSpots = {
+    { x = 1231.824, y = 346.6726, z = 81.99091 },
+}
---
+++ resources/[esx]/andreas_hambrant/items.sql
@@ -0,0 +1,8 @@
+USE `essentialmode`;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('turbojast', 'Turbojäst', 1, 0, 1),
+	('socker', 'Socker', 1, 0, 1),
+  ('kol', 'Värmekol', 1, 0, 1),
+  ('corn', 'Majs', 1, 0, 1)
+;
