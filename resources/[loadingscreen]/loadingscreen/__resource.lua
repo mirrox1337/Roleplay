8a94b8d2cdfe8c85e8cd6733b322d79fe693265d
Mirrox1337 <anton.per.larsson@gmail.com>
2019-12-26 17:55:32 +0100

    loadingscreen

--- resources/[loadingscreen]/loadingscreen/__resource.lua
+++ resources/[loadingscreen]/loadingscreen/__resource.lua
@@ -1,10 +1,22 @@
+client_script "client.lua"
+
 files {
-    'index.html',
-    'style.css',
-    'img/logo.png',
-    'img/background.jpg'
+  'index.html',
+  '1.css',
+  'logo.png',
+  'music/music.ogg',
+  'app.js',
+  'imgs/bg0.jpg',
+  'imgs/bg1.jpg',
+  'imgs/bg2.jpg',
+  'imgs/bg3.jpg',
+  'imgs/bg4.jpg',
+  'imgs/bg5.jpg',
+  'imgs/bg6.jpg',
+  'imgs/bg7.jpg',
 }
 
+loadscreen_manual_shutdown "yes"
 loadscreen 'index.html'
 
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
